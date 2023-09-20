0fb19bff9ed7fa6ef4c77c539ea24cf73df023c8
Jack <43528012+jackk25@users.noreply.github.com>
2023-09-20 14:02:27 -0700

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       jackk25, RobotKayla                                       */
+/*    Created:      Wed Sep 20 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
