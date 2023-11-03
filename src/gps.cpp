8f33aee2b46cbff122a3563f98bc512847ee2d66
Kayla <26-kmcculloch@seaprep.org>
2023-11-03 13:56:02 -0700

    new gps code

--- src/gps.cpp
+++ src/gps.cpp
@@ -3,6 +3,15 @@
 
 using namespace vex;
 
+int printPosition(){
+
+  Brain.Screen.print(GPS15.xPosition(inches));
+  Brain.Screen.print(", ");
+  Brain.Screen.print(GPS15.yPosition(inches));
+
+  return 0;
+}
+
 int driveToPoint() {
   Controller1.Screen.setCursor(1, 1);
 
@@ -33,6 +42,28 @@ int driveToPoint() {
   //instructs robot to drive to new position
   mainDrive.turnFor(right, robotAngle, degrees);
   mainDrive.driveFor(forward, w, inches);
-
+  
   return 0;
 }
+
+int hopeThisWorks(){
+  //me literally converting a coding blocks example from the vex website into c++
+  //https://kb.vex.com/hc/en-us/articles/360061932711-Using-the-V5-GPS-Sensor
+  Controller1.Screen.setCursor(1, 1);
+ 
+  //calibrates the GPS
+  GPS15.calibrate();
+
+  GPS15.setLocation(0, 0, inches, 0, degrees);
+  printPosition();
+
+  Brain.Screen.setCursor(3, 1);
+
+  while(GPS15.xPosition(inches) < 30 || GPS15.yPosition(inches) < 30){
+    mainDrive.drive(forward);
+    Brain.Screen.setCursor(3, 1);
+    printPosition();
+  }
+  return 0;
+}
+
