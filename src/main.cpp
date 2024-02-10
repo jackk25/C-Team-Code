69fcc860565d30fbd31d13bb194346d54ab68c54
Jack <43528012+jackk25@users.noreply.github.com>
2024-02-10 09:26:43 -0800

    New Autons

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "vex.h"
 #include "screen.h"
 #include "img.h"
+#include "vex_global.h"
 
 using namespace vex;
 
@@ -23,10 +24,11 @@ motor intake = motor(PORT9, ratio18_1, false);
 motor launcher = motor(PORT20, ratio6_1, true);
 
 bool skills = false;
+bool mirror = false;
 
 void autonControl(){
-  if (skills == false){
-    Brain.Screen.print("Competition!");
+  if (skills == false && mirror == false){
+    Brain.Screen.print("Competition Regular!");
     mainDrive.driveFor(forward, 46, inches);
     mainDrive.turnFor(right, 45, degrees);
     mainDrive.driveFor(forward, 6, inches);
@@ -38,30 +40,57 @@ void autonControl(){
     mainDrive.setDriveVelocity(50, pct);
     mainDrive.driveFor(reverse, 12, inches);
     intake.spin(forward);
+    //go back
+    mainDrive.turnFor(right, 50, deg);
+    mainDrive.driveFor(forward, 46, inches);
+    mainDrive.turnFor(right, 45, degrees);
+    mainDrive.driveFor(forward, 12*3.25, inches);
+  }
+  if (skills == false && mirror == true){
+    Brain.Screen.print("Competition Mirrored!");
+    mainDrive.driveFor(forward, 46, inches);
+    mainDrive.turnFor(left, 45, degrees);
+    mainDrive.driveFor(forward, 6, inches);
+    intake.spin(reverse);
+    mainDrive.driveFor(reverse, 12, inches);
+    //DESTROY THE GOAL!!!!
+    mainDrive.setDriveVelocity(75, pct);
+    mainDrive.driveFor(forward, 20, inches);
+    mainDrive.setDriveVelocity(50, pct);
+    mainDrive.driveFor(reverse, 12, inches);
+    intake.spin(forward);
+    //go back
+    mainDrive.turnFor(left, 50, deg);
+    mainDrive.driveFor(forward, 46, inches);
+    mainDrive.turnFor(left, 45, degrees);
+    mainDrive.driveFor(forward, 12*3.25, inches);
   }
   if (skills){
     Brain.Screen.print("Skills!");
     launcher.setVelocity(100, pct);
-    launcher.spin(forward);
+    launcher.spinFor(forward, 45, seconds);
+    mainDrive.driveFor(forward, 14, inches);
+    mainDrive.turnFor(left, 70, degrees);
+    mainDrive.driveFor(forward, 12, inches);
+    mainDrive.turnFor(left, 90, degrees);
+    mainDrive.driveFor(forward, 16, inches);
+    intake.spinFor(reverse, 3, seconds);
+    mainDrive.driveFor(reverse, 12, inches);
+    mainDrive.turnFor(right, 100, degrees);
+    mainDrive.driveFor(reverse, 30, inches);
+    mainDrive.turnFor(right, 80, degrees);
+    mainDrive.driveFor(forward, 54, inches);
+    mainDrive.turnFor(left, 90, degrees);
+    mainDrive.driveFor(forward, 30, inches);
+    mainDrive.turnFor(right, 90, degrees);
+    mainDrive.driveFor(forward,15, inches);
+    mainDrive.driveFor(reverse, 12, inches);
+    mainDrive.turnFor(right, 45, degrees);
+    mainDrive.driveFor(forward, 4, inches);
+    mainDrive.turnFor(left, 45, degrees);
+    mainDrive.driveFor(forward, 24, inches);
   }
 }
-
-void possibleAuton(){
-  intake.spinFor(left, 3, seconds);
-  mainDrive.turnFor(left, 45, degrees);
-  mainDrive.driveFor(forward, 6, inches);
-  leftWing.set(true);
-  mainDrive.turnFor(right, 10, degrees);
-  mainDrive.driveFor(forward, 6, inches);
-  mainDrive.turnFor(right, 35, degrees);
-  mainDrive.driveFor(forward, 12, inches);
-  intake.spinFor(reverse, 3, seconds);
-  mainDrive.driveFor(reverse, 15, inches);
-  mainDrive.turnFor(right, 90, degrees);
-  mainDrive.driveFor(forward, 32, inches);
-}
-
-void possible 
 
 void motorSpin(motor_group dstMotor, bool positiveButtonState, bool negativeButtonState, int spinRate){
   int motorSpeed = (positiveButtonState * spinRate) - (negativeButtonState * spinRate);
@@ -88,7 +117,7 @@ void driveCode() {
     rightDrive.spin(forward, rightDriveStrength, pct);
 
     motorSpin(intake, Controller1.ButtonL1.pressing(), Controller1.ButtonL2.pressing(), 100);
-    motorSpin(launcher, false, Controller1.ButtonR1.pressing(), 85);
+    motorSpin(launcher, false, Controller1.ButtonR1.pressing(), 100);
     
     if(Controller1.ButtonUp.pressing()){
       leftWing.set(true);
@@ -106,6 +135,12 @@ void switchAuton(int buttonId){
   Controller1.rumble(".");
   if(buttonId <= 3){
     skills = false;
+  }
+  if(buttonId == 1 || buttonId == 2){
+    mirror = false;
+  }
+  if(buttonId == 3 || buttonId == 0){
+    mirror = true;
   }
   if(buttonId == 4){
     skills = true;
@@ -126,12 +161,16 @@ void createButtons(){
 
   //Auton Control
   Brain.Screen.setFillColor(redColor);
-  container.createButton({42, 238}, switchAuton); //id: 0 
-  container.createButton({42, 60}, switchAuton); //id: 1
+  container.createButton({42, 238}, switchAuton, "0"); //id: 0 
 
   Brain.Screen.setFillColor(blueColor);
-  container.createButton({224, 238}, switchAuton); //id: 2
-  container.createButton({224, 60}, switchAuton); //id: 3
+  container.createButton({42, 60}, switchAuton, "1"); //id: 1
+
+  Brain.Screen.setFillColor(redColor);
+  container.createButton({224, 238}, switchAuton, "2"); //id: 2
+
+  Brain.Screen.setFillColor(blueColor);
+  container.createButton({224, 60}, switchAuton, "3"); //id: 3
 
   Brain.Screen.setFillColor(purpleColor);
   container.createButton({352, 163}, switchAuton); //id: 4
